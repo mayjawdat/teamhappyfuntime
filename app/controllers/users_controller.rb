f7902983c230adf1ca3e6745fab25f8daa49a000
May Jawdat & Ransom Byers <pair+mayjawdat.rasnom@devbootcamp.com>
2017-04-20 16:06:25 -0700

    Add errors partial and use in registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      @errors = @user.errors.full_messages
+      render 'new'
     end
   end
 
