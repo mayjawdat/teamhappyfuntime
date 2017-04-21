ddb7139d66857723dbf6abbb26fe25973c332f04
May Jawdat & Ransom Byers <pair+mayjawdat.rasnom@devbootcamp.com>
2017-04-20 19:42:06 -0700

    Add activerecord associations to connect users and skills

--- db/schema.rb
+++ db/schema.rb
@@ -48,12 +48,12 @@ ActiveRecord::Schema.define(version: 20170421002420) do
   end
 
   create_table "users_skills", force: :cascade do |t|
-    t.integer  "skills_id"
-    t.integer  "users_id"
+    t.integer  "skill_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["skills_id"], name: "index_users_skills_on_skills_id", using: :btree
-    t.index ["users_id"], name: "index_users_skills_on_users_id", using: :btree
+    t.index ["skill_id"], name: "index_users_skills_on_skill_id", using: :btree
+    t.index ["user_id"], name: "index_users_skills_on_user_id", using: :btree
   end
 
 end
