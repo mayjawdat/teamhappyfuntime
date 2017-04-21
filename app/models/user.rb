29fbf69ce90b51c96fc93a6a0e0be5da872051be
Dylan Metas <dylanmetas@gmail.com>
2017-04-21 15:44:48 -0700

    add validator for phone and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, :full_name, :password_digest,  presence: true
   validates :is_mentor, inclusion: [true, false]
-
+  validates :phone, presence: true, unless: ->(user){user.email.present?}
+  validates :email, presence: true, unless: ->(user){user.phone.present?}
+  
   has_many :users_skills
   has_many :skills, through: :users_skills
 
