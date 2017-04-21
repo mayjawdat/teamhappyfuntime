78e1694519eddadc55b32e2a744ecbe8d4af6c1a
Brianna Forster & Ransom Byers <pair+b-forster.rasnom@devbootcamp.com>
2017-04-21 12:06:56 -0700

    Fix hidden student_id field on signup form

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -24,6 +24,7 @@ class AppointmentsController < ApplicationController
 
   def update
     @appointment = Appointment.find(params[:id])
+    @appointment.update(student_id: appointment_params[:student_id])
 
     if @appointment.save
       redirect_to current_user
@@ -37,6 +38,6 @@ class AppointmentsController < ApplicationController
   private
 
   def appointment_params
-    params.require(:appointment).permit(:start_time, :end_time)
+    params.require(:appointment).permit(:start_time, :end_time, :student_id)
   end
 end
